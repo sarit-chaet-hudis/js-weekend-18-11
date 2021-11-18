545618c4216e4f0ccca70bd6465a2aa7a8b3ad22
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-18 17:19:46 +0200

    6-1 mumbling

--- 6-1-mumbling.js
+++ 6-1-mumbling.js
@@ -1,3 +1,11 @@
-function accum (str) {
-    return str;
+function accum(str) {
+  const strArray = str.split("");
+  console.log(strArray);
+  const result = [];
+  strArray.forEach((char, index) => {
+    result.push(char.toUpperCase() + char.toLowerCase().repeat(index));
+  });
+  return result.join("-");
 }
+
+console.log(accum("cwAT"));
